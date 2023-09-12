bcde51234e351a8a48f28b66ccf29561739ef5da
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 21:25:39 +0600

    Add project files.

---
+++ Mod_2_6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mod_2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите см = ");
+            int sm = Convert.ToInt32(Console.ReadLine());
+            int metr = sm / 100;
+            Console.WriteLine("Число полных метров: " + metr);
+        }
+    }
+}
